292b0d9abc52b41dfca925559f7021eb4b657759
vivomo <929992114@qq.com>
2023-10-12 17:58:15 +0800

    feat: update Official website

--- packages/client/src/pages/home/index.tsx
+++ packages/client/src/pages/home/index.tsx
@@ -9,10 +9,12 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css';
+import FAQ from '@/config/faq';
 
 const Home = () => {
 
   const [roomId, setRoomId] = useState('');
+  const [faqActiveIndex, setFaqActiveIndex] = useState(-1);
   const [username, setUsername] = useState('');
   const [avatar, setAvatar] = useState<null | string>(null);
   const navigate = useNavigate();
@@ -86,26 +88,19 @@ const Home = () => {
           <section className={"mi-section mi-section-faq"}>
             <h2>Frequently Asked Questions</h2>
             <div className="faq-wrapper">
-              <dl>
-                <dt>How to play?</dt>
-                <dd>As a player, you start each round by drawing a character card. Each card comes with random attributes like health, attack, speed, and equipment capacity. In the Alpha test, card draws are on the house – no charge. This NFT card is your ticket to the game. Once you're in, you can earn points by digging or plundering opponents for treasures on the map and hauling them out of the tunnel entrance.</dd>
-              </dl>
-              <dl>
-                <dt>How to dig treasures?</dt>
-                <dd>Players will enter a randomly generated map with pre-placed treasures. Different treasures have different point values. Once you successfully extract treasures out of the cave, you will be rewarded with points for this match. In the game, your movement speed is directly affected by your starting attributes and load, so you must decide when to start transporting treasure out of the cave based on how much treasure you've found.</dd>
-              </dl>
-              <dl>
-                <dt>What is the battling system?</dt>
-                <dd>Inside the cave, the player can initiate a battle against another character within attack range. Battles will be turn-based, with the player having the option to attack or run away, and the outcome  will be based on the behavioral choices of both players, as well as their strategy choices in relation to each other. The loser of the battle will lose all items in their backpack after death, and then the player will be teleported out of the cave and re-randomize their character.</dd>
-              </dl>
-              <dl>
-                <dt>How long will a match last?</dt>
-                <dd>Each season is estimated to last 3~5 days. Within the same season, players can enter the cave multiple times and bring out the treasure multiple times to gain points, after the season is over we will count the players' points to rank them, and players at the top of the list will be rewarded with bounties.</dd>
-              </dl>
-              <dl>
-                <dt>What is the leaderboard?</dt>
-                <dd>Gold diggers, the loot from the cave will earn you points, and the top ten warriors in accumulated points will receive a bounty reward!</dd>
-              </dl>
+              {
+                FAQ.map((item, index) => {
+                  return (
+                    <dl
+                      key={item.title}
+                      onClick={() => setFaqActiveIndex(faqActiveIndex === index ? -1 : index)}
+                    >
+                      <dt className={faqActiveIndex === index ? 'active' : ''}>{item.title}</dt>
+                      <dd>{item.content}</dd>
+                    </dl>
+                  )
+                })
+              }
             </div>
           </section>
         </SwiperSlide>
