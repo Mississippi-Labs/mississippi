79480b45a422439a77d4380ef42b0c72297f51a7
vivomo <929992114@qq.com>
2023-10-16 21:07:00 +0800

    feat: landing page frame

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Game from './pages/game';
 import Test from './pages/test';
+import Home from '@/pages/home';
 
 export const App = () => {
 
@@ -10,8 +11,8 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Game />} />
-            {/*<Route index element={<Home />} />*/}
+            {/*<Route index element={<Game />} />*/}
+            <Route index element={<Home />} />
             <Route path="game" element={<Game />} />
             <Route path="test" element={<Test />} />
           </Route>
---
+++ packages/client/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '@/pages/home/header';
+import './styles.scss';
+
+const Home = () => {
+  return (
+    <div className="mi-home-page">
+      <Header/>
+    </div>
+  );
+};
+
+export default Home;
