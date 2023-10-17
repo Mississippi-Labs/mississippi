6b770d2f489be8967d1737ed6f24a5d8b7033330
vivomo <929992114@qq.com>
2023-10-17 21:42:40 +0800

    feat: game's fog add opt btn

--- packages/client/src/components/UserAvatar/index.tsx
+++ packages/client/src/components/UserAvatar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.scss';
+import UserAddress from '@/components/UserAddress';
 
 interface IProps {
   avatar: string;
@@ -13,19 +14,21 @@ interface IProps {
 
 const UserAvatar = (props: IProps) => {
 
-  const { avatar, username, hp, maxHp, ap, maxAp, roomId } = props;
+  const { avatar, username = 'user', hp, maxHp, ap, maxAp, roomId } = props;
 
   return (
     <div className="mi-c-user-avatar">
       <div className={`avatar-box avatar-${avatar}`}/>
+
+      <UserAddress address={'0x12324b4in4lkn2424124421'} account={'0.1ETH'}/>
+      {/*<div className="hp-wrapper">*/}
+      {/*  <div className="hp" style={{ width: `${hp / maxHp * 100}%` }}/>*/}
+      {/*</div>*/}
+      {/*<div className="ap-wrapper">*/}
+      {/*  <div className="ap" style={{ width: `${ap / maxAp * 100}%` }}/>*/}
+      {/*</div>*/}
       <div className="username">{username}</div>
-      <div className="hp-wrapper">
-        <div className="hp" style={{ width: `${hp / maxHp * 100}%` }}/>
-      </div>
-      <div className="ap-wrapper">
-        <div className="ap" style={{ width: `${ap / maxAp * 100}%` }}/>
-      </div>
-      <div className="room-id">Room ID: {roomId}</div>
+      {/*<div className="room-id">Room ID: {roomId}</div>*/}
     </div>
   );
 };
