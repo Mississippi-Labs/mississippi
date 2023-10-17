cdb51acf7dec129675e299f556669e2601c7caa1
vivomo <929992114@qq.com>
2023-10-17 20:07:28 +0800

    feat: add copy function

--- packages/client/src/components/UserAddress/index.tsx
+++ packages/client/src/components/UserAddress/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import copyImg from '@/assets/img/copy.png';
 import './styles.scss';
+import { message } from 'antd';
 
 interface IProps {
   account: string;
@@ -9,12 +12,21 @@ interface IProps {
 const UserAddress = (props: IProps) => {
 
   const { account, address = '' } = props;
-  const addressTxt = `${address.slice(0, 6)}...${address.slice(-4)}`
+  const addressTxt = `${address.slice(0, 6)}...${address.slice(-4)}`;
+
+  const onCopy = () => {
+    message.success('Copy successfully');
+  }
 
   return (
     <div className="user-address-wrapper">
       <div className="user-account">{account}</div>
-      <div className="user-address">{addressTxt}</div>
+      <CopyToClipboard text={address} onCopy={onCopy}>
+        <div className="user-address">{addressTxt}</div>
+      </CopyToClipboard>
+      <CopyToClipboard text={address} onCopy={onCopy}>
+        <img src={copyImg} alt="" className="copy-icon"/>
+      </CopyToClipboard>
     </div>
   );
 };
