56cf0fc33692ec1eb1785bc8da7a9355eec9c029
LidamaoHub <lee-perfect@qq.com>
2023-11-04 21:02:48 +0800

    remove limit

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,50 +1,44 @@
 import { setup } from "./mud/setup";
 import mudConfig from "contracts/mud.config";
 import ReactDOM from "react-dom/client";
-import { App } from './App';
-import './common.scss';
-import { world } from '@/mud/world';
+import { App } from "./App";
+import "./common.scss";
+import { world } from "@/mud/world";
 import { MUDProvider } from "@/mud/MUDContext";
-const {
-  network,
-} = await setup();
+const { network } = await setup();
 
 // Components expose a stream that triggers when the component is updated.
-
 
 const rootElement = document.getElementById("root");
 setup().then(async (result) => {
   root.render(
     <MUDProvider value={result}>
-      <App/>
+      <App />
     </MUDProvider>
-)
+  );
 });
 if (!rootElement) throw new Error("React root not found");
 
 const root = ReactDOM.createRoot(rootElement);
 
-
 // https://vitejs.dev/guide/env-and-mode.html
-if (true) {
-  const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
-  const comp = [];
-  network.world.components.forEach((c) => {
-    if (comp.findIndex((item => item.id === c.id)) === -1) {
-      comp.push(c);
-    }
-  });
-  network.world.components = comp;
-  mountDevTools({
-    config: mudConfig,
-    publicClient: network.publicClient,
-    walletClient: network.walletClient,
-    latestBlock$: network.latestBlock$,
-    blockStorageOperations$: network.blockStorageOperations$,
-    worldAddress: network.worldContract.address,
-    worldAbi: network.worldContract.abi,
-    write$: network.write$,
-    recsWorld: network.world,
-  });
-  console.log("mounted")
-}
+const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
+const comp = [];
+network.world.components.forEach((c) => {
+  if (comp.findIndex((item) => item.id === c.id) === -1) {
+    comp.push(c);
+  }
+});
+network.world.components = comp;
+mountDevTools({
+  config: mudConfig,
+  publicClient: network.publicClient,
+  walletClient: network.walletClient,
+  latestBlock$: network.latestBlock$,
+  blockStorageOperations$: network.blockStorageOperations$,
+  worldAddress: network.worldContract.address,
+  worldAbi: network.worldContract.abi,
+  write$: network.write$,
+  recsWorld: network.world,
+});
+console.log("mounted");
