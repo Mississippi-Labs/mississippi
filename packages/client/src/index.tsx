439e93e829344c821c5e6a2a52e2d92fe213d336
Tiyo <tiyosheng@gmail.com>
2023-11-05 14:40:57 +0800

    fix: fix mud tool

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -7,38 +7,33 @@ import { world } from "@/mud/world";
 import { MUDProvider } from "@/mud/MUDContext";
 const { network } = await setup();
 
-// Components expose a stream that triggers when the component is updated.
-
 const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("React root not found");
+const root = ReactDOM.createRoot(rootElement);
+
 setup().then(async (result) => {
   root.render(
     <MUDProvider value={result}>
       <App />
     </MUDProvider>
   );
+  const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
+  const comp = [];
+  network.world.components.forEach((c) => {
+    if (comp.findIndex((item) => item.id === c.id) === -1) {
+      comp.push(c);
+    }
+  });
+  network.world.components = comp;
+  mountDevTools({
+    config: mudConfig,
+    publicClient: network.publicClient,
+    walletClient: network.walletClient,
+    latestBlock$: network.latestBlock$,
+    blockStorageOperations$: network.blockStorageOperations$,
+    worldAddress: network.worldContract.address,
+    worldAbi: network.worldContract.abi,
+    write$: network.write$,
+    recsWorld: network.world,
+  });
 });
-if (!rootElement) throw new Error("React root not found");
-
-const root = ReactDOM.createRoot(rootElement);
-
-// https://vitejs.dev/guide/env-and-mode.html
-const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
-const comp = [];
-network.world.components.forEach((c) => {
-  if (comp.findIndex((item) => item.id === c.id) === -1) {
-    comp.push(c);
-  }
-});
-network.world.components = comp;
-mountDevTools({
-  config: mudConfig,
-  publicClient: network.publicClient,
-  walletClient: network.walletClient,
-  latestBlock$: network.latestBlock$,
-  blockStorageOperations$: network.blockStorageOperations$,
-  worldAddress: network.worldContract.address,
-  worldAbi: network.worldContract.abi,
-  write$: network.write$,
-  recsWorld: network.world,
-});
-console.log("mounted");
