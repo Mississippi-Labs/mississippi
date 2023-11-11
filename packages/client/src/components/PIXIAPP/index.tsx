7f95dfb44a85a14cedfd090efd762a747cd5ca74
vivomo <929992114@qq.com>
2023-11-11 22:24:46 +0800

    feat: preview path & delivery

--- packages/client/src/components/PIXIAPP/index.tsx
+++ packages/client/src/components/PIXIAPP/index.tsx
@@ -4,6 +4,8 @@ import { MapConfig } from '@/config/map';
 import PIXIMap from '@/components/PIXIMap';
 import Chests from '@/components/Chests';
 import { TreasureChestMockData } from '@/mock/data';
+import Delivery from '@/components/Delivery';
+import PreviewPaths from '@/components/PreviewPaths';
 
 const { cellSize, spriteCellSize, visualWidth, visualHeight } = MapConfig;
 
@@ -15,7 +17,28 @@ const PIXIAPP = () => {
       options={{ resolution: 1 }}
     >
       <PIXIMap/>
+      <Delivery/>
+      <PreviewPaths
+        data={[
+          {
+            x: 5,
+            y: 5,
+            movable: true
+          },
+          {
+            x: 5,
+            y: 6,
+            movable: true
+          },
+          {
+            x: 5,
+            y: 7
+          },
+        ]}
+      />
       <Chests data={TreasureChestMockData}/>
+
+
     </Stage>
   );
 };
