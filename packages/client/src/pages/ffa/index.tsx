c5275171dadf60e932a7d41020fb40b149c3353f
vivomo <929992114@qq.com>
2023-12-11 18:15:46 +0800

    feat: ffa page

--- packages/client/src/pages/ffa/index.tsx
+++ packages/client/src/pages/ffa/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './styles.scss';
+import Header from './header';
+import UserInfo from './userInfo';
 
 const FFA = () => {
   return (
-    <div>
+    <div className={'ffa-page'}>
+      <Header/>
 
+      <section className={'ffa-section'}>
+        <div className="ffa-switch-wrapper">
+          <h2 className={'switch-item active'}>Home</h2>
+          <h2 className={'switch-item'}>Battle</h2>
+        </div>
+        <UserInfo/>
+        <button className="mi-btn">Mint and Go</button>
+      </section>
     </div>
   );
 };
