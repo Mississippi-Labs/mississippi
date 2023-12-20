21576f84aec17c225854b42220e5768d23936be5
vivomo <929992114@qq.com>
2023-12-20 08:37:11 +0800

    feat: mock data

--- packages/client/src/mock/data.ts
+++ packages/client/src/mock/data.ts
@@ -95,3 +95,64 @@ export const TreasureChestMockData: ITreasureChest[] = [
   }
 ];
 
+const hunterEquip = {
+  head: 'Demon Crown',
+  clothes: 'Demon Husk',
+  handheld: 'Demon\'s Hands'
+}
+
+const warriorEquip = {
+  head: 'Ancient Helm',
+  clothes: 'Holy Chestplate',
+  handheld: 'Holy Gauntlets'
+}
+
+export const playerA = {
+  "x": 0.5,
+  "y": 1.5,
+  "hp": 115,
+  "attack": 46,
+  "attackRange": 4,
+  "speed": 4,
+  "strength": 35,
+  "space": 3,
+  "oreBalance": 0,
+  "treasureBalance": 0,
+  "state": 2,
+  "lastBattleTime": 0,
+  "maxHp": 115,
+  "name": "V2",
+  "url": "",
+  "addr": "0xb53c83ef2467da36c687c81cb23140d92e3d10ba",
+  "username": "V2",
+  "equip": warriorEquip,
+  "waiting": false,
+  "action": "idle",
+  "moving": false,
+  "toward": "Right"
+}
+
+export const playerB = {
+  "x": 5.1,
+  "y": 1.5,
+  "hp": 135,
+  "attack": 40,
+  "attackRange": 3,
+  "speed": 4,
+  "strength": 32,
+  "space": 3,
+  "oreBalance": 0,
+  "treasureBalance": 0,
+  "state": 2,
+  "lastBattleTime": 0,
+  "maxHp": 135,
+  "name": "V",
+  "url": "",
+  "addr": "0xb58fd9cb0c9100bb6694a4d18627fb238d3bb893",
+  "username": "V",
+  "equip": hunterEquip,
+  "waiting": false,
+  "action": "idle",
+  "moving": false,
+  "toward": "Left"
+}
--- packages/client/src/pages/testBattle/index.tsx
+++ packages/client/src/pages/testBattle/index.tsx
@@ -4,68 +4,8 @@ import Player from '@/components/PIXIPlayers/Player';
 import DuelField, { AttackType, IDuelFieldMethod } from '@/components/DuelField';
 
 import './styles.scss';
+import { playerA, playerB } from '@/mock/data';
 
-const hunterEquip = {
-  head: 'Demon Crown',
-  clothes: 'Demon Husk',
-  handheld: 'Demon\'s Hands'
-}
-
-const warriorEquip = {
-  head: 'Ancient Helm',
-  clothes: 'Holy Chestplate',
-  handheld: 'Holy Gauntlets'
-}
-
-const playerA = {
-  "x": 0.5,
-  "y": 1.5,
-  "hp": 115,
-  "attack": 46,
-  "attackRange": 4,
-  "speed": 4,
-  "strength": 35,
-  "space": 3,
-  "oreBalance": 0,
-  "treasureBalance": 0,
-  "state": 2,
-  "lastBattleTime": 0,
-  "maxHp": 115,
-  "name": "V2",
-  "url": "",
-  "addr": "0xb53c83ef2467da36c687c81cb23140d92e3d10ba",
-  "username": "V2",
-  "equip": warriorEquip,
-  "waiting": false,
-  "action": "idle",
-  "moving": false,
-  "toward": "Right"
-}
-
-const playerB = {
-  "x": 5.1,
-  "y": 1.5,
-  "hp": 135,
-  "attack": 40,
-  "attackRange": 3,
-  "speed": 4,
-  "strength": 32,
-  "space": 3,
-  "oreBalance": 0,
-  "treasureBalance": 0,
-  "state": 2,
-  "lastBattleTime": 0,
-  "maxHp": 135,
-  "name": "V",
-  "url": "",
-  "addr": "0xb58fd9cb0c9100bb6694a4d18627fb238d3bb893",
-  "username": "V",
-  "equip": hunterEquip,
-  "waiting": false,
-  "action": "idle",
-  "moving": false,
-  "toward": "Left"
-}
 
 const TestBattle = () => {
 
